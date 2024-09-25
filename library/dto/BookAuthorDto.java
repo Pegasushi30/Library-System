0b8f20c0e1d06392154a7d4b0c30c9c375c79b96
Burak Eymen Çevik <121224269+Pegasushi30@users.noreply.github.com>
2024-09-25 05:29:36 +0300

    Update BookAuthorDto.java

--- library/dto/BookAuthorDto.java
+++ library/dto/BookAuthorDto.java
@@ -3,7 +3,7 @@ package com.project.library.dto;
 import java.util.UUID;
 
 public record BookAuthorDto(
-        UUID id, // UUID kullanılıyor
+        UUID id,
         Integer bookIsbn,
         AuthorDto author
 ) {}
